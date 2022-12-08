6cea7b6395eb1331a6aefc75751ad34fa579794c
Blessingobulor <blessingpeters24@gmail.com>
2022-12-08 11:39:33 -0800

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                                    
 
-/**
- * main - Print a string
- * Description: use printf
- * Return: 0
- */
-int main(void)
-{
-	printf("%S", "with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+                                                                                                                                      
+
+/**                                                                                                                                   
+ 
+   * main - Print a string                                                                                                              
+ 
+    * Description: use printf                                                                                                            
+ 
+     * Return: 0                                                                                                                          
+ 
+      */                                                                                                                                   
+
+int main(void)                                                                                                                        
+
+{                                                                                                                                   
+
+	        printf("%s", "with proper grammar, but the outcome is a piece of art, \n");                                                           return (0);                                                                
 }
