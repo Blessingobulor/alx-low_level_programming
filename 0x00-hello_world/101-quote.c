cc940130a60826f73a8dec54c827426b14a7a09e
Blessingobulor <blessingpeters24@gmail.com>
2022-12-08 18:53:22 -0800

    but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"and that piece of art is useful"
+ * Return 1
+ */
+
+int main(void)
+{
+	puts("\"and that piece of art is useful");
+	return (1);
