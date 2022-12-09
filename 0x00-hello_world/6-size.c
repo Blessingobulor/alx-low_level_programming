477ab91fc2d1d5ff2cb43b503b2723307b1ae25d
Blessingobulor <blessingpeters24@gmail.com>
2022-12-09 11:39:05 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**                                             
- * main -Prints sized of characters
+ *main -Prints sized of characters
  *
  *Return: Always 0 (success)
  */
