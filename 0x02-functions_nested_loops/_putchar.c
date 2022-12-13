2e889ad7d5752f916fec3bbc30dd26a267f6ec83
Blessingobulor <blessingpeters24@gmail.com>
2022-12-13 10:39:34 -0800

    task1

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,11 +3,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: the character to print 
  *
  * Return: on success 1
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1)):
+	return (write(1, &c, 1));
+
 }
