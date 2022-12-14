6af05fc6c6e6763d126133068a3134836d02e5e3
Blessingobulor <blessingpeters24@gmail.com>
2022-12-13 21:06:26 -0800

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,47 @@
-void print_times_table(int n) {
-  if (n < 0 || n > 15) {
-    return;
-  }
-  for (int i = 0; i <= 10; i++) {
-    printf(“%d * %d = %d\n”, n, i, n * i);
-  }
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table -  function that prints the multiplication table
+ * @n: user value
+ */
+void print_times_table(int n)
+{
+	int a;
+	int b;
+	int c;
+	if (n < 0 || n >= 15)
+		return;
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			c = (a * b);
+			if (b == 0)
+				_putchar('0' + c);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (c <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + c);
+				}
+				else if (c > 9 && c < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (c / 10));
+					_putchar('0' + (c % 10));
+				}
+				else if (c >= 100)
+				{
+					_putchar('0' + (c / 100));
+					_putchar('0' + ((c / 10) % 10));
+					_putchar('0' + (c % 10));
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
