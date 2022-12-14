47c6a3ed1a8c346cfaa25c6489a3f65ed668ba37
Blessingobulor <blessingpeters24@gmail.com>
2022-12-13 21:03:15 -0800

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,26 +1,21 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
- * Return: 0
+ * print_to_98 - function dat prints natural nums followed by a new line
+ * @n: number provided by user
  */
-int main(void)
+void print_to_98(int n)
 {
-	int c = 0;
-	int sum = 0;
-
-	while (c < 1024)
+	if (n < 98)
 	{
-		if (c % 3 == 0 || c % 5 == 0)
-		{
-			sum += c;
-		}
-
-		c++;
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
 	}
-	printf("%i\n", sum);
-	return (0);
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
 }
-
