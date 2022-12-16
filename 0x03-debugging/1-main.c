60617fc62719a0e7355e2a80c85a58653e43dc9b
Blessingobulor <blessingpeters24@gmail.com>
2022-12-15 16:11:09 -0800

    task0

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,13 @@ int main(void)
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
+	/**
 
-	while (i < 10)
-	{
-		putchar(i);
-	}
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
 
 	printf("infinite loop avoided! \\0/\n");
 
