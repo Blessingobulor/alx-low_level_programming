0656079fee87976200255dfd823419a11b073985
Blessingobulor <blessingpeters24@gmail.com>
2022-12-19 10:36:39 -0800

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a newline
+ * @s: The string to be printed
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*(s + length) != '\0')
+		length++;
+
+	while (length > 0)
+	{
+		_putchar(*(s + length - 1));
+		length--;
+	}
+	b10_pow(10);
+
+	_putchar('\n');
+}
+
