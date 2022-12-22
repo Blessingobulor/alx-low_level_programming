f1210f9ba58f563523e809942fa3ab72a9d07664
Blessingobulor <blessingpeters24@gmail.com>
2022-12-21 19:23:24 -0800

    task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,49 +1,48 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer
- * @s: The string to be converted
- * Return: The first integer in the string
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	short sign = 1;
-	double result = 0;
-	int len, digits_seen_count, i, pass, positive, old_sign;
+	int i, d, n, len, f, digit;
 
-	len = i = digits_seen_count = pass = old_sign = 0;
-	while (pass <= 1)
-	{
-		positive = old_sign != 0 ? old_sign == *(s + len) : *(s + len) == '+';
-		if (*(s + len) == '\0')
-		{
-			pass++;
-			len = 0;
-			continue;
-		}
-		else if (*(s + len) >= '0' && *(s + len) <= '9')
-		{
-			if (pass == 0)
-				digits_seen_count++;
-			else
-				result += (*(s + len) - '0') * b10_pow(digits_seen_count - i++ - 1);
-		}
-		else
-		{
-			if (pass == 0 && digits_seen_count > 0)
-			{
-				pass++;
-				len -= digits_seen_count;
-				continue;
-			}
-			if ((*(s + len) == '+' || *(s + len) == '-') && digits_seen_count == 0)
-			{
-				sign = positive ? 1 : 0 - 1;
-				old_sign = positive ? '+' : '-';
-			}
-		}
+	i = 0;
+
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
 		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	return (result * sign);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
 
