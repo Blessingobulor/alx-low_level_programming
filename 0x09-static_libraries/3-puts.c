a95d1d1546b12198388838ad370c33de531e20c2
Blessingobulor <blessingpeters24@gmail.com>
2023-01-06 21:43:58 -0800

    static

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout
+ *
+ * @str: a parameter to _puts function
+ */
+void _puts(char *str)
+{
+for (; *str != '\0'; str++)
+{
+_putchar (*str);
+}
+_putchar ('\n');
+}
