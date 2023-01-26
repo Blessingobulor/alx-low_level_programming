9b2252776cc206ce677db12f9613fb3fdcb54ab6
Blessingobulor <blessingpeters24@gmail.com>
2023-01-26 01:31:31 -0800

    print

--- 0x10-variadic_functions/main.h
+++ 0x10-variadic_functions/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-_putchar(char c);
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
